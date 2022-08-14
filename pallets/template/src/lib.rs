aa5f2a47f22e977205b8397ef717430e6152c473
baronPortalverse <portalverse@aol.com>
2022-08-15 04:01:04 +0800

    big improve

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -19,6 +19,17 @@ pub mod pallet {
 	use frame_support::pallet_prelude::{*, ValueQuery};
 	use frame_system::pallet_prelude::*;
 
+	/// To represent a rendering instance
+	#[derive(Encode, Decode, Clone, PartialEq, Eq, Default, TypeInfo, Debug)]
+	pub struct Renderer<BlockNumber> {
+		// ipv6 or ipv4(u32)
+		pub host: u128,
+		pub portoffset: u16,
+		pub reg_at: BlockNumber,
+		pub status: u8,
+		// TODO POV staking
+	}
+
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -27,6 +38,7 @@ pub mod pallet {
 	}
 
 	#[pallet::pallet]
+	#[pallet::without_storage_info]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
@@ -42,6 +54,21 @@ pub mod pallet {
 	#[pallet::getter(fn something2)]
 	pub type Something2<T:Config> = StorageDoubleMap<_,Blake2_128Concat, u32, Blake2_128Concat, T::AccountId, u32,ValueQuery>;
 
+	/// Storage of all renderers.
+    /// The storage does not guarante that the host:port of a renderer is unique,
+    /// some extra constraints must be applied
+    #[pallet::storage]
+	#[pallet::getter(fn renderers)]
+    pub type Renderers<T: Config> = StorageDoubleMap<
+        _,
+        Blake2_128Concat,
+        u32,
+        Blake2_128Concat,
+        T::AccountId,
+        Renderer<T::BlockNumber>,
+        OptionQuery,
+    >;
+
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
@@ -50,6 +77,10 @@ pub mod pallet {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		SomethingStored(u32, T::AccountId),
+		RendererRegistered(T::AccountId),
+		RendererDeregistered(T::AccountId),
+		RendererConnected(T::AccountId,T::AccountId),
+		RendererDisconnected(T::AccountId,T::AccountId),
 	}
 
 	// Errors inform users that something went wrong.
@@ -59,6 +90,7 @@ pub mod pallet {
 		NoneValue,
 		/// Errors should have helpful documentation associated with them.
 		StorageOverflow,
+		AlreadyRegistered,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -69,7 +101,7 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn do_somethingggg(origin: OriginFor<T>, something: u32) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://docs.substrate.io/v3/runtime/origins
@@ -100,6 +132,80 @@ pub mod pallet {
 			Ok(())
 		}
 
+
+		/// Register a renderer. A basic challenge should be initiated immediately by the offchain workers.
+		#[pallet::weight(10_000)]
+		pub fn register(origin: OriginFor<T>, region: u32, host: u128, portoffset: u16) -> DispatchResultWithPostInfo {
+			let renderer = ensure_signed(origin)?;
+			Renderers::<T>::try_mutate(&region, &renderer, |exists| -> DispatchResult {
+				ensure!(exists.is_none(), Error::<T>::AlreadyRegistered);
+				exists.replace(Renderer {
+					host,
+					portoffset,
+					reg_at: frame_system::Pallet::<T>::block_number(),
+					status: 0,
+				});
+				Ok(())
+			})?;
+			Self::deposit_event(Event::RendererRegistered(renderer));
+			Ok(().into())
+		}
+
+
+		/// deregister a renderer. 
+		#[pallet::weight(10_000)]
+		pub fn deregister(origin: OriginFor<T>, region: u32) -> DispatchResultWithPostInfo {
+			let renderer = ensure_signed(origin)?;
+			Renderers::<T>::remove(&region, &renderer);
+			Self::deposit_event(Event::RendererDeregistered(renderer));
+			Ok(().into())
+		}
+
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn connect(
+			origin: OriginFor<T>,region: u32,
+			renderer: T::AccountId,
+			// TODO add stablecoin payment
+		) -> DispatchResult {
+			let player = ensure_signed(origin)?;
+			Renderers::<T>::try_mutate(&region, &renderer, |exists| -> DispatchResult {
+				ensure!(!exists.is_none(), Error::<T>::AlreadyRegistered);
+				exists.replace(Renderer {
+					host:exists.as_ref().unwrap().host,
+					portoffset:exists.as_ref().unwrap().portoffset,
+					reg_at: exists.as_ref().unwrap().reg_at,
+					status: 1,
+				});
+				Ok(())
+			})?;
+			// TODO pay for the connection
+			Self::deposit_event(Event::RendererConnected(renderer,player));
+			Ok(())
+		}
+
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn disconnect(origin: OriginFor<T>, region: u32, renderer: T::AccountId) -> DispatchResult {
+			let player = ensure_signed(origin)?;
+			Renderers::<T>::try_mutate(&region, &renderer, |exists| -> DispatchResult {
+				ensure!(!exists.is_none(), Error::<T>::AlreadyRegistered);
+				exists.replace(Renderer {
+					host:exists.as_ref().unwrap().host,
+					portoffset:exists.as_ref().unwrap().portoffset,
+					reg_at: exists.as_ref().unwrap().reg_at,
+					status: 0,
+				});
+				Ok(())
+			})?;
+			// TODO pay for the connection
+			Self::deposit_event(Event::RendererDisconnected(renderer,player));
+			Ok(())
+		}
+
+		
+
+
 		/// An example dispatchable that may throw a custom error.
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
@@ -119,4 +225,6 @@ pub mod pallet {
 			}
 		}
 	}
+
+
 }
