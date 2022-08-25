5f05b705fb3b1dfbf407ac6d93be1d43056d2eee
baronPortalverse <portalverse@aol.com>
2022-08-25 16:23:27 +0800

    add gameid

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -17,16 +17,18 @@ mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::pallet_prelude::{*, ValueQuery};
+	use frame_support::inherent::Vec;
 	use frame_system::pallet_prelude::*;
-
+	
 	/// To represent a rendering instance
 	#[derive(Encode, Decode, Clone, PartialEq, Eq, Default, TypeInfo, Debug)]
 	pub struct Renderer<BlockNumber> {
 		// ipv6 or ipv4(u32)
-		pub host: u128,
+		pub host: Vec<u8>,
 		pub portoffset: u16,
 		pub reg_at: BlockNumber,
 		pub status: u8,
+		pub gameid: Vec<u8>,
 		// TODO POV staking
 	}
 
@@ -42,17 +44,17 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	// // The pallet's runtime storage items.
+	// // https://docs.substrate.io/v3/runtime/storage
+	// #[pallet::storage]
+	// #[pallet::getter(fn something)]
+	// // Learn more about declaring storage items:
+	// // https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
+	// pub type Something<T> = StorageValue<_, u32>;
 
-	#[pallet::storage]
-	#[pallet::getter(fn something2)]
-	pub type Something2<T:Config> = StorageDoubleMap<_,Blake2_128Concat, u32, Blake2_128Concat, T::AccountId, u32,ValueQuery>;
+	// #[pallet::storage]
+	// #[pallet::getter(fn something2)]
+	// pub type Something2<T:Config> = StorageDoubleMap<_,Blake2_128Concat, u32, Blake2_128Concat, T::AccountId, u32,ValueQuery>;
 
 	/// Storage of all renderers.
     /// The storage does not guarante that the host:port of a renderer is unique,
@@ -100,42 +102,42 @@ pub mod pallet {
 	impl<T: Config> Pallet<T> {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_somethingggg(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
+		// #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		// pub fn do_somethingggg(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		// 	// Check that the extrinsic was signed and get the signer.
+		// 	// This function will return an error if the extrinsic is not signed.
+		// 	// https://docs.substrate.io/v3/runtime/origins
+		// 	let who = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
+		// 	// Update storage.
+		// 	<Something<T>>::put(something);
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
+		// 	// Emit an event.
+		// 	Self::deposit_event(Event::SomethingStored(something, who));
+		// 	// Return a successful DispatchResultWithPostInfo
+		// 	Ok(())
+		// }
 
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something2(origin: OriginFor<T>, something1: u32, something2: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
+		// #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		// pub fn do_something2(origin: OriginFor<T>, something1: u32, something2: u32) -> DispatchResult {
+		// 	// Check that the extrinsic was signed and get the signer.
+		// 	// This function will return an error if the extrinsic is not signed.
+		// 	// https://docs.substrate.io/v3/runtime/origins
+		// 	let who = ensure_signed(origin)?;
 
-			// Update storage.
-			Something2::<T>::insert(something1,who.clone(),something2);
+		// 	// Update storage.
+		// 	Something2::<T>::insert(something1,who.clone(),something2);
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something1, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
+		// 	// Emit an event.
+		// 	Self::deposit_event(Event::SomethingStored(something1, who));
+		// 	// Return a successful DispatchResultWithPostInfo
+		// 	Ok(())
+		// }
 
 
 		/// Register a renderer. A basic challenge should be initiated immediately by the offchain workers.
 		#[pallet::weight(10_000)]
-		pub fn register(origin: OriginFor<T>, region: u32, host: u128, portoffset: u16) -> DispatchResultWithPostInfo {
+		pub fn register(origin: OriginFor<T>, region: u32, host: Vec<u8>, portoffset: u16, games: Vec<u8>) -> DispatchResultWithPostInfo {
 			let renderer = ensure_signed(origin)?;
 			Renderers::<T>::try_mutate(&region, &renderer, |exists| -> DispatchResult {
 				ensure!(exists.is_none(), Error::<T>::AlreadyRegistered);
@@ -144,6 +146,7 @@ pub mod pallet {
 					portoffset,
 					reg_at: frame_system::Pallet::<T>::block_number(),
 					status: 0,
+					gameid: games,
 				});
 				Ok(())
 			})?;
@@ -172,10 +175,11 @@ pub mod pallet {
 			Renderers::<T>::try_mutate(&region, &renderer, |exists| -> DispatchResult {
 				ensure!(!exists.is_none(), Error::<T>::AlreadyRegistered);
 				exists.replace(Renderer {
-					host:exists.as_ref().unwrap().host,
+					host:exists.as_ref().unwrap().host.clone(),
 					portoffset:exists.as_ref().unwrap().portoffset,
 					reg_at: exists.as_ref().unwrap().reg_at,
 					status: 1,
+					gameid: exists.as_ref().unwrap().gameid.clone(),
 				});
 				Ok(())
 			})?;
@@ -191,10 +195,11 @@ pub mod pallet {
 			Renderers::<T>::try_mutate(&region, &renderer, |exists| -> DispatchResult {
 				ensure!(!exists.is_none(), Error::<T>::AlreadyRegistered);
 				exists.replace(Renderer {
-					host:exists.as_ref().unwrap().host,
+					host:exists.as_ref().unwrap().host.clone(),
 					portoffset:exists.as_ref().unwrap().portoffset,
 					reg_at: exists.as_ref().unwrap().reg_at,
 					status: 0,
+					gameid: exists.as_ref().unwrap().gameid.clone(),
 				});
 				Ok(())
 			})?;
@@ -203,27 +208,27 @@ pub mod pallet {
 			Ok(())
 		}
 
-		
 
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
+		// /// An example dispatchable that may throw a custom error.
+		// #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
+		// pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
+		// 	let _who = ensure_signed(origin)?;
+
+		// 	// Read a value from storage.
+		// 	match <Something<T>>::get() {
+		// 		// Return an error if the value has not been set.
+		// 		None => Err(Error::<T>::NoneValue)?,
+		// 		Some(old) => {
+		// 			// Increment the value read from storage; will error in the event of overflow.
+		// 			let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+		// 			// Update the value in storage with the incremented result.
+		// 			<Something<T>>::put(new);
+		// 			Ok(())
+		// 		},
+		// 	}
+		// }
 	}
 
 
